4c6ac89e64c4f35e06af58c8c2486ead9181e115
tjdbssy137 <65931605+tjdbssy137@users.noreply.github.com>
2025-11-28 01:35:59 +0900

    [서윤] 초상화 애니메이션3

--- Assets/@Game/Domain/WorldObjects/Portrait/UI_Portrait.cs
+++ Assets/@Game/Domain/WorldObjects/Portrait/UI_Portrait.cs
@@ -5,15 +5,40 @@ using DG.Tweening;
 
 public class UI_Portrait : UI_Base
 {
+    private Animator _animator;
+    private enum States
+    {
+        None,
+        Fine,
+        Embarrased,
+        Panic,
+        Faint,
+    }
+    float _center = 0;
+    float _panic = 0;
+    bool _faint = false;
+    private States _state = States.Fine;
     public override bool Init()
     {
         if (base.Init() == false)
 			return false;
 
-        this.UpdateAsObservable().Subscribe(_=>
+        _animator = this.GetComponentInChildren<Animator>();
+        if (_animator == null)
         {
-            
-        }).AddTo(this);
+            Debug.LogWarning("_animator is NULL");
+        }
+
+        this.UpdateAsObservable()
+        .Subscribe(_=>
+        {   
+            if(Contexts.InGame.IsGameOver)
+            {
+                _animator.SetTrigger("Faint");
+                return;
+            }
+            UpdatePortrait();
+        }).AddTo(this);        
 
 		return true;
     }
@@ -27,4 +52,52 @@ public class UI_Portrait : UI_Base
         
 
     }
+
+    private void UpdatePortrait()
+    {
+        if(Contexts.InGame.AKey)
+        {
+            _center = -1;   
+        }
+        else if(Contexts.InGame.DKey)
+        {
+            _center = 1;         
+        }
+        else
+        {
+            _center = 0;
+        }
+
+        
+        _animator.SetFloat("Center", _center);
+        _animator.SetFloat("Panic", _panic);
+        // switch(_state)
+        // {
+        //     case States.Fine:
+        //     {
+                    
+        //     }
+        //     break;
+        //     case States.Embarrased:
+        //     {
+                    
+        //     }
+        //     break;
+        //     case States.Panic:
+        //     {
+                    
+        //     }
+        //     break;
+        //     case States.Faint:
+        //     {
+                    
+        //     }
+        //     break;
+        //     case States.None:
+        //     break;
+        // }
+    }
+
+
+
 }
