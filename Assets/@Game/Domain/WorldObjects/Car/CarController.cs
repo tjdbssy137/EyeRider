978f504cd9a9b0a3ff8a237775383652cac878b6
tjdbssy137 <65931605+tjdbssy137@users.noreply.github.com>
2025-11-28 22:04:36 +0900

    [서윤] 코드 정리

--- Assets/@Game/Domain/WorldObjects/Car/CarController.cs
+++ Assets/@Game/Domain/WorldObjects/Car/CarController.cs
@@ -96,65 +96,7 @@ public partial class CarController : BaseObject
             _center = _rigidbody.position;
         }
 
-        Contexts.InGame.OnEnterCorner
-            .Subscribe(deg =>
-            {
-                _rigidbody.linearVelocity = Vector3.zero;
-                _rigidbody.angularVelocity = Vector3.zero;
-
-                Vector3 f = transform.forward;
-                f.y = 0f;
-                transform.forward = f.normalized;
-
-                Vector3 r = transform.right;
-                r.y = 0f;
-                transform.right = r.normalized;
-
-                _pendingRotation = true;
-                _pendingDegrees = deg;
-
-                Observable.Timer(TimeSpan.FromSeconds(0.1f))
-                    .Subscribe(_ =>
-                    {
-                        if (_pendingRotation)
-                        {
-                            _pendingRotation = false;
-                            this.Steer(_pendingDegrees);
-                        }
-                    })
-                    .AddTo(_disposables);
-            })
-            .AddTo(_disposables);
-
-
-        Contexts.InGame.OnExitEye
-            .Subscribe(dist =>
-            {
-                _isOutside = true;
-                _lastDistance= dist;
-                _distancePanic = Mathf.Clamp01(_lastDistance/100);
-            })
-            .AddTo(_disposables);
-
-        Contexts.InGame.OnEnterEye
-            .Subscribe(_ =>
-            {
-                _isOutside = false;
-                _lastDistance = 0;
-                _animator.SetFloat("Distance", 0f);
-            })
-            .AddTo(_disposables);
-
-        Observable.Interval(TimeSpan.FromSeconds(0.2f))
-            .Subscribe(_ =>
-            {
-                if (true == _isOutside)
-                {
-                    float dmg = DistancePenalty(_lastDistance);
-                    Contexts.InGame.Car.DamageCondition(dmg);
-                }
-            })
-            .AddTo(_disposables);
+        BindSubscriptions();
 
         this.FixedUpdateAsObservable()
             .Subscribe(_ =>
@@ -177,35 +119,6 @@ public partial class CarController : BaseObject
                 Accelerate();
             })
             .AddTo(_disposables);
-
-        Contexts.InGame.OnCollisionObstacle
-        .Subscribe(_ =>
-        {
-            Contexts.InGame.IsCollisionObstacle++;
-            _eventPanic += 0.2f; // 임시값
-
-            Observable.Timer(TimeSpan.FromSeconds(1.2f))
-                .Subscribe(_ =>
-                {
-                    _eventPanic -= 0.2f;
-                    Contexts.InGame.IsCollisionObstacle--;
-                    Contexts.InGame.IsCollisionObstacle = Mathf.Max(0, Contexts.InGame.IsCollisionObstacle);
-                })
-                .AddTo(_disposables);
-        })
-        .AddTo(_disposables);
-
-        Contexts.InGame.Car.OnConditionChanged
-        .Subscribe(newCondition =>
-        {
-            _conditionPanic = Mathf.Clamp01(1 - newCondition.Item2/100);
-        }).AddTo(this);
-
-        Contexts.InGame.Car.OnFuelChanged
-        .Subscribe(newFuel =>
-        {
-            _fuelPanic = Mathf.Clamp01(1 - newFuel.Item2/100);
-        }).AddTo(this);
 
         return true;
     }
